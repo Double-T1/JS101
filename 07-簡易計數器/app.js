e0e53614ca42e33fa87ba460992f914c8bce7072
chen weichen <newsforcwc@gmail.com>
2024-06-29 17:27:48 +0800

    added functions for the plus and minus button, making the counter reflect users actions. yet it never goes below 0

--- 07-簡易計數器/app.js
+++ 07-簡易計數器/app.js
@@ -1,1 +1,19 @@
 // 程式碼寫在這裡
+//while clicking + and - button, 
+//the value in between change accordingly
+//yet it never becomes smaller than 0
+
+document.addEventListener("DOMContentLoaded", () => {
+    const number = document.querySelector("#counter"); 
+    const plus = document.querySelector("#plus");
+    plus.addEventListener("click", () => {
+        number.value = Number(number.value) + 1;
+    })
+    const minus = document.querySelector("#minus");
+    minus.addEventListener("click", () => {
+        let cv = Number(number.value);
+        if (cv > 0) {
+            number.value = cv - 1;
+        }
+    })
+});

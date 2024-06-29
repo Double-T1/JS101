0b565c140cfdce598a62d0889bd51b9333b6549a
chen weichen <newsforcwc@gmail.com>
2024-06-29 17:14:18 +0800

    finished the BMI calculator, made it functional

--- 07-BMI計算機/app.js
+++ 07-BMI計算機/app.js
@@ -1,2 +1,12 @@
 // 程式碼寫在這裡
 // 提示：BMI = 體重(kg) / 身高(m) 平方
+document.addEventListener("DOMContentLoaded",() => {
+    const button = document.querySelector("button");
+
+    button.addEventListener("click", () => {
+        const h = Number(document.querySelector("#bodyHeight").value)/100;
+        const w = Number(document.querySelector("#bodyWeight").value);
+        const BMI = w / (h**2);
+        document.querySelector("#resultText").textContent = BMI.toFixed(2); //toFIxed converts the element into a string
+    })
+});

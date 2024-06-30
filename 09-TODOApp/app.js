c934daf6982a879cc8b22ff95a51ac7acb172853
chen weichen <newsforcwc@gmail.com>
2024-06-30 16:26:35 +0800

    can add and close tasks at will now

--- 09-TODOApp/app.js
+++ 09-TODOApp/app.js
@@ -1,1 +1,33 @@
 // 程式碼寫這裡
+document.addEventListener("DOMContentLoaded",() => {
+    // two goals,
+    // 1. add new task onto the todo list,
+    // 2. delete finished task from the list 
+
+    //goal 2
+    function addEventClose(button) {
+        button.addEventListener("click",function (){
+            this.parentNode.remove();
+        })
+    } 
+    document.querySelectorAll(".closeBtn").forEach((button) => {
+        addEventClose(button);   
+    })
+
+    // goal 1
+    const baseNode = document.querySelector(".todo-item").cloneNode(true);
+    const createLi = () => {
+        const newLi = baseNode.cloneNode(true);
+        addEventClose(newLi.childNodes[3]);
+        return newLi;
+    }
+
+    const addButton = document.querySelector("#addBtn");
+    addButton.addEventListener("click", () => {
+        const newTask = document.querySelector("#taskInput");
+        const list = document.querySelector(".todo-list");
+        const li = createLi();
+        li.childNodes[1].textContent = newTask.value
+        list.insertAdjacentElement("afterbegin",li);
+    })
+})

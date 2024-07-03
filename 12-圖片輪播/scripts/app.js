722b3102d2b6571795abb3aa0a08059f662f151b
chen weichen <newsforcwc@gmail.com>
2024-07-03 16:15:31 +0800

    can browse picture through left right button, or the nav indicator below

--- 12-圖片輪播/scripts/app.js
+++ 12-圖片輪播/scripts/app.js
@@ -1,1 +1,71 @@
 // 程式碼寫這裡
+document.addEventListener("DOMContentLoaded", () => {
+    let currentPic = 0;
+    const slides = document.querySelectorAll(".slide");
+    const prevButton = document.querySelector(".prev-btn");
+    const nextButton = document.querySelector(".next-btn");
+    const indi = document.querySelectorAll(".indicator");
+
+    //helper functions
+    const manageNav = function() {
+        indi[currentPic].classList.add("active"); 
+    }
+
+    const manageButton = function() {
+        if (currentPic === 0) {
+            prevButton.style.display = "none";
+        } else if (currentPic === 1) {
+            prevButton.style.display = "block";
+        } 
+        
+        if (currentPic === slides.length-1) {
+            nextButton.style.display = "none";
+        } else if (currentPic === slides.length-2) {
+            nextButton.style.display = "block";
+        }
+    }
+
+    const changeSlide = function() {
+        document.querySelector(".slide-track").innerHTML = `<li class="slide">${slides[currentPic].innerHTML}</li>`;
+        manageButton();
+        manageNav();
+    }
+
+    const createChangePic = function(target) {
+        const changePic = function() {
+            indi[currentPic].classList.remove("active");
+            currentPic = target;
+            changeSlide();
+        }
+        return changePic;
+    }
+
+    const toNextPic = function() {
+        if (currentPic<slides.length-1) {
+            indi[currentPic].classList.remove("active");
+            currentPic += 1;
+            changeSlide();
+        } 
+    }
+
+    const toPrevPic = function() {
+        if (currentPic>0) {
+            indi[currentPic].classList.remove("active");
+            currentPic -= 1;
+            changeSlide();
+        } 
+    }
+    
+    //logic starts
+    changeSlide();
+
+    prevButton.addEventListener("click",toPrevPic);
+    nextButton.addEventListener("click",toNextPic);
+
+    //switch indicator
+    indi.forEach((ele,index) => {
+        ele.addEventListener("click",createChangePic(index));
+    })
+})
+
+

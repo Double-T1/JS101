c9225edd3efdef07418aa563b792b01e224fb0e7
chen weichen <newsforcwc@gmail.com>
2024-07-04 14:23:18 +0800

    added defer keyword at the insertion of script in html

--- 12-圖片輪播/scripts/app.js
+++ 12-圖片輪播/scripts/app.js
@@ -1,71 +1,67 @@
 // 程式碼寫這裡
-document.addEventListener("DOMContentLoaded", () => {
-    let currentPic = 0;
-    const slides = document.querySelectorAll(".slide");
-    const prevButton = document.querySelector(".prev-btn");
-    const nextButton = document.querySelector(".next-btn");
-    const indi = document.querySelectorAll(".indicator");
+let currentPic = 0;
+const slides = document.querySelectorAll(".slide");
+const prevButton = document.querySelector(".prev-btn");
+const nextButton = document.querySelector(".next-btn");
+const indi = document.querySelectorAll(".indicator");
 
-    //helper functions
-    const manageNav = function() {
-        indi[currentPic].classList.add("active"); 
-    }
+//helper functions
+const manageNav = function() {
+    indi[currentPic].classList.add("active"); 
+}
 
-    const manageButton = function() {
-        if (currentPic === 0) {
-            prevButton.style.display = "none";
-        } else {
-            prevButton.style.display = "block";
-        } 
-        
-        if (currentPic === slides.length-1) {
-            nextButton.style.display = "none";
-        } else {
-            nextButton.style.display = "block";
-        }
-    }
-
-    const changeSlide = function() {
-        document.querySelector(".slide-track").innerHTML = `<li class="slide">${slides[currentPic].innerHTML}</li>`;
-        manageButton();
-        manageNav();
-    }
-
-    const createChangePic = function(target) {
-        const changePic = function() {
-            indi[currentPic].classList.remove("active");
-            currentPic = target;
-            changeSlide();
-        }
-        return changePic;
-    }
-
-    const toNextPic = function() {
-        if (currentPic<slides.length-1) {
-            indi[currentPic].classList.remove("active");
-            currentPic += 1;
-            changeSlide();
-        } 
-    }
-
-    const toPrevPic = function() {
-        if (currentPic>0) {
-            indi[currentPic].classList.remove("active");
-            currentPic -= 1;
-            changeSlide();
-        } 
-    }
+const manageButton = function() {
+    if (currentPic === 0) {
+        prevButton.style.display = "none";
+    } else {
+        prevButton.style.display = "block";
+    } 
     
-    //logic starts
-    changeSlide();
+    if (currentPic === slides.length-1) {
+        nextButton.style.display = "none";
+    } else {
+        nextButton.style.display = "block";
+    }
+}
 
-    prevButton.addEventListener("click",toPrevPic);
-    nextButton.addEventListener("click",toNextPic);
+const changeSlide = function() {
+    document.querySelector(".slide-track").innerHTML = `<li class="slide">${slides[currentPic].innerHTML}</li>`;
+    manageButton();
+    manageNav();
+}
 
-    //switch indicator
-    indi.forEach((ele,index) => {
-        ele.addEventListener("click",createChangePic(index));
-    })
+const createChangePic = function(target) {
+    const changePic = function() {
+        indi[currentPic].classList.remove("active");
+        currentPic = target;
+        changeSlide();
+    }
+    return changePic;
+}
+
+const toNextPic = function() {
+    if (currentPic<slides.length-1) {
+        indi[currentPic].classList.remove("active");
+        currentPic += 1;
+        changeSlide();
+    } 
+}
+
+const toPrevPic = function() {
+    if (currentPic>0) {
+        indi[currentPic].classList.remove("active");
+        currentPic -= 1;
+        changeSlide();
+    } 
+}
+
+//logic starts
+changeSlide();
+
+prevButton.addEventListener("click",toPrevPic);
+nextButton.addEventListener("click",toNextPic);
+
+//switch indicator
+indi.forEach((ele,index) => {
+    ele.addEventListener("click",createChangePic(index));
 })
-
-
